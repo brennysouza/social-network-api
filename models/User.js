ed1a5c54f85b5373042a961ec87ded1b446b1b7f
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-10-17 14:59:47 -0400

    removed reaction schema in models, not needed

--- models/User.js
+++ models/User.js
@@ -1,1 +1,39 @@
 const mongoose = require('mongoose');
+
+// models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/\S+@\S+\.\S+/, 'is invalid'],
+  },
+  thoughts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Thought',
+    },
+  ],
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+});
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
