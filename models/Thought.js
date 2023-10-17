73e344804705e18be9aac55b9e7b69e3ea0289d3
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-10-17 15:14:28 -0400

    added schema for thoughts

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,31 @@
 const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAt) => {
+          return new Date(createdAt).toISOString();
+        },
+    },
+    userName: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    reactions: [reactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+    id: false,
+});
