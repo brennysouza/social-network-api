c3945df6bd900c4f855a02c736b6931a46d3d101
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-10-18 13:43:29 -0400

    added dotenv file and working on MONGODBURI connection

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+module.exports = mongoose.connection;
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 
@@ -17,4 +19,9 @@ app.use('/api', routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to the database');
 });
